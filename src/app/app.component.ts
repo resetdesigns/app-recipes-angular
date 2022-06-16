612880e40bba06906e76b3fbb96813f61a9c91b4
Joseph Villajin <resetdesigns@gmail.com>
2022-06-16 14:36:31 -0700

    Passed Data with Event Emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app-recipes-angular';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  // TODO: Clean up inline styling. Move to global stylesheet on encapsulate within component styles.
 }
