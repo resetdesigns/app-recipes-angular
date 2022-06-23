7737c9779f3d806ea471c376f211399e244c2ff7
Joseph Villajin <resetdesigns@gmail.com>
2022-06-23 11:26:18 -0700

    Created Ability to Update Ingredient Information

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -34,10 +34,20 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.shoppingListService.addIngredient(newIngredient);
+    if (this.editMode === true) {
+      this.shoppingListService.updateIngredient(
+        this.editedItemIndex,
+        newIngredient
+      );
+    } else {
+      this.shoppingListService.addIngredient(newIngredient);
+    }
+
+    form.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy(): void {
