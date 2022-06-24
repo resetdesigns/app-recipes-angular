c08f0f0b6eb912ccc29790267c44f653a92d9c1c
Joseph Villajin <resetdesigns@gmail.com>
2022-06-24 16:32:37 -0700

    Moved Recipes to External Source

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,22 +12,30 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
   // set to private so this cannot be overwritten outside of the service
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super tasty schnitzel for your enjoyment.',
-      'https://1r6s7piqcip1uwgh2vlwrt15-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/TNWC-Article-Cover-Image.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Big Burger',
-      'Juicy burger with all the fillings',
-      'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/5:4/w_3129,h_2503,c_limit/Smashburger-recipe-120219.jpg',
-      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+
+  // Recipes are set via database
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super tasty schnitzel for your enjoyment.',
+  //     'https://1r6s7piqcip1uwgh2vlwrt15-wpengine.netdna-ssl.com/wp-content/uploads/2020/06/TNWC-Article-Cover-Image.jpg',
+  //     [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Big Burger',
+  //     'Juicy burger with all the fillings',
+  //     'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/5:4/w_3129,h_2503,c_limit/Smashburger-recipe-120219.jpg',
+  //     [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
