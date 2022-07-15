3e97f16eba84e57931eab1effed0649221c64d11
Joseph Villajin <resetdesigns@gmail.com>
2022-07-15 10:01:11 -0700

    Added auto-login features to NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from './store/app.reducer';
+import * as AuthActions from './auth/store/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,9 @@ export class AppComponent implements OnInit {
   // TODO: Clean up inline styling. Move to global stylesheet on encapsulate within component styles.
   // TODO: Fix "Manage Recipe" dropdown. Dropdown Directive is not available in recipe module.
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit(): void {
-    this.authService.autoLogin();
+    this.store.dispatch(new AuthActions.AutoLogin());
   }
 }
